95aa4da9e7cf2623967de875ca8a66011946085e
Wisanu Prayoonsuk <secwind.dev@gmail.com>
2024-12-11 12:25:53 +0700

    feat: add ADC and GlobalPageStorage exports with usage examples in index.ts

--- src/index/index.ts
+++ src/index/index.ts
@@ -1,4 +1,6 @@
-// export * from '../directive'
+import HTTP from '../http/ADC'
+import GlobalPageStorage from '../http/GlobalPageStorage'
+
 export * from '../fnArray'
 export * from '../fnCheck'
 export * from '../fnCi'
@@ -9,6 +11,50 @@ export * from '../fnRun'
 export * from '../fnService'
 export * from '../fnTo'
 export * from '../fnValidate'
-export * from '../http/ADC'
-export * from '../http/GlobalPageStorage'
 export * from '../http/type-http'
+
+/**
+ * สร้าง Fetch API สำหรับใช้งาน HTTP requests
+ * @example const adc = new ADC<Req, Res>()
+ * @example const response = adc.request(config)
+ */
+export const ADC = HTTP
+
+/**
+ * สร้าง instance ของ GlobalPageStorage
+ * สามารถใช้เรียกใช้งาน storage ได้ทุกที่ในโปรเจค
+ * โดยไม่ต้องสร้าง instance ใหม่ และจะถูกล้างข้อมูลทั้งหมดเมื่อหน้าเว็บปิด
+ */
+export const GPS = GlobalPageStorage.getInstance()
+
+// interface Req {
+//     id?: number
+//     name?: string
+// }
+
+// interface Res {
+//     data?: {
+//         id: number
+//         name: string
+//     }
+//     error?: string
+// }
+
+// const config = {
+//     baseURL: 'http://api.test.com',
+//     method: 'GET',
+//     storage: 'cache' as const,
+//     variables: { id: 1 },
+//     name: 'data',
+//     timeToLive: 60000, // 1 minute
+// } as const
+
+// const api = new adc<Req, Res>()
+// const firstResponse = api.request({
+//     baseURL: 'http://api.test.com',
+//     method: 'GET',
+//     storage: 'cache' as const,
+//     variables: { id: 1 },
+//     name: 'data',
+//     timeToLive: 60000, // 1 minute
+// })
